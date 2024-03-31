3752128352dd83feec38b133a230d697ddec198d
mustafayilmazdev <mustafaylmz173@gmail.com>
2024-03-31 12:15:05 +0300

    add get entries sql

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -1,12 +1,8 @@
--- CREATE TABLE "entries" (
---   "id" bigserial PRIMARY KEY,
---   "account_id" bigint NOT NULL,
---   "amount" bigint NOT NULL,
---   "created_at" timestamptz NOT NULL DEFAULT (now())
--- );
-
 -- name: CreateEntry :one
 INSERT INTO entries (account_id, amount) VALUES ($1,$2) RETURNING *;
 
 -- name: GetEntry :one
 SELECT * FROM entries WHERE id=$1 LIMIT 1;
+
+-- name: GetEntries :many
+SELECT * FROM entries WHERE account_id=$1 ORDER BY id LIMIT $2 OFFSET $3;
--- db/sqlc/entry.sql.go
+++ db/sqlc/entry.sql.go
@@ -39,6 +39,44 @@ func (q *Queries) CreateEntry(ctx context.Context, arg CreateEntryParams) (Entry
 	return i, err
 }
 
+const getEntries = `-- name: GetEntries :many
+SELECT id, account_id, amount, created_at FROM entries WHERE account_id=$1 ORDER BY id LIMIT $2 OFFSET $3
+`
+
+type GetEntriesParams struct {
+	AccountID int64 `json:"account_id"`
+	Limit     int32 `json:"limit"`
+	Offset    int32 `json:"offset"`
+}
+
+func (q *Queries) GetEntries(ctx context.Context, arg GetEntriesParams) ([]Entry, error) {
+	rows, err := q.db.QueryContext(ctx, getEntries, arg.AccountID, arg.Limit, arg.Offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Entry
+	for rows.Next() {
+		var i Entry
+		if err := rows.Scan(
+			&i.ID,
+			&i.AccountID,
+			&i.Amount,
+			&i.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
 const getEntry = `-- name: GetEntry :one
 SELECT id, account_id, amount, created_at FROM entries WHERE id=$1 LIMIT 1
 `
