cbf653f4aafaad5ce8acf3537217f25e5192d98b
mustafayilmazdev <mustafaylmz173@gmail.com>
2024-06-23 08:04:59 +0300

    edit store test print

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -79,8 +79,8 @@ func TestTransferTx(t *testing.T) {
 		require.NotEmpty(t, toAccount)
 		require.Equal(t, account2.ID, toAccount.ID)
 
-		//TODO check accounts balance
-		fmt.Println(">> tx: ", account1.Balance, account2.Balance)
+		// check balances
+		fmt.Println(">> tx :", fromAccount.Balance, toAccount.Balance)
 
 		diff1 := account1.Balance - fromAccount.Balance
 		diff2 := toAccount.Balance - account2.Balance
