a5966291ce3e6391da0cead1fd0ee877ec6f30d1
mustafayilmazdev <mustafaylmz173@gmail.com>
2024-06-23 16:39:36 +0300

    add accounts endpoint to insert account

---
+++ api/account.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/mustafayilmazdev/simplebank/db/sqlc"
+)
+
+type CreateAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req CreateAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+	fmt.Println("acc:: ", arg)
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
---
+++ api/server.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/mustafayilmazdev/simplebank/db/sqlc"
+)
+
+// * Note [codermuss]: Server serves HTTP requests for our banking service.
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// * Note [codermuss]: NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+	router.POST("/accounts", server.createAccount)
+	server.router = router
+	return server
+}
+
+// * Note [codermuss]: Starts the HTTP server on a specific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
