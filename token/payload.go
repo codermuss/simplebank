f181e9726f94f1970daf16aab637e65f48578ac3
mustafayilmazdev <mustafaylmz173@gmail.com>
2024-06-25 04:42:15 +0300

    add paseto and jwt

--- token/payload.go
+++ token/payload.go
@@ -1,13 +1,18 @@
 package token
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
 )
 
-// * Note [codermuss]: Payload containts the payload data of the token
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
 
+// * Note [codermuss]: Payload containts the payload data of the token
 type Payload struct {
 	ID        uuid.UUID `json:"id"`
 	Username  string    `json:"username"`
@@ -30,4 +35,12 @@ func NewPayload(username string, duration time.Duration) (*Payload, error) {
 
 	return payload, nil
 
+}
+
+// * Note [codermuss]: Valid checks if the token payload is valid or not
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
 }
